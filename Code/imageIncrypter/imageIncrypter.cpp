23ea65af356bfba55d363157a342d112977cca7a
crussell2021 <50681298+crussell2021@users.noreply.github.com>
2020-01-23 00:28:17 -0800

    ran some code

--- Code/imageIncrypter/imageIncrypter.cpp
+++ Code/imageIncrypter/imageIncrypter.cpp
@@ -13,56 +13,120 @@ int binToDecimal(bool*);
 int powerOf(int base, int exponent);
 
 int lastNBits(int value, int nBits);
+int removeNBits(int value, int numBits);
 
 int main()
 {
-	int n = 7;
+	int n = 2;
 	//read in original
-	std::string inputFileName = "stockTree.ppm";
-	std::ifstream inFile;
-	inFile.open(inputFileName.c_str(), std::ios::in);
+	std::string originalInputFileName = "stockTree.ppm";
+	std::ifstream originalFile;
+	originalFile.open(originalInputFileName.c_str(), std::ios::in);
 
-	if (inFile.fail()) {
-		std::cout << "Failed to open file: " << inputFileName << std::endl;
+	if (originalFile.fail()) {
+		std::cout << "Failed to open file: " << originalInputFileName << std::endl;
 		return -1;
 	}
 
-	std::string	inputHeading;
+	std::string	originalInputHeading;
 	std::string	p3Heading = "P3";
 	std::string	p2Heading = "P2";
-	inFile >> inputHeading;
-	if (inputHeading != p3Heading && inputHeading != p2Heading) {
-		std::cout << "Heading: " << inputHeading << " not supported." << std::endl;
+	originalFile >> originalInputHeading;
+	if (originalInputHeading != p3Heading && originalInputHeading != p2Heading) {
+		std::cout << "Heading: " << originalInputHeading << " not supported." << std::endl;
 		return -1;
 	}
 
-	int width, height, pixelCount, maxVal;
-	inFile >> width >> height >> maxVal;;
-	pixelCount = width * height;
-	std::cout << "Reading " + inputHeading + " file with width: " << width << " and height: " << height << std::endl;
+	int originalWidth, originalHeight, originalPixelCount, maxVal;
+	originalFile >> originalWidth >> originalHeight >> maxVal;;
+	originalPixelCount = originalWidth * originalHeight;
+	std::cout << "Reading " + originalInputHeading + " file with width: " << originalWidth << " and originalHeight: " << originalHeight << std::endl;
 
-	maxVal = powerOf(2, n);
+	//read in hidden
+	std::string hiddenInputFileName = "meme.ppm";
+	std::ifstream hiddenFile;
+	hiddenFile.open(hiddenInputFileName.c_str(), std::ios::in);
 
-	if (inputHeading == p3Heading) {
-		rgb* pixelArray = new rgb[pixelCount];
+	if (hiddenFile.fail()) {
+		std::cout << "Failed to open file: " << hiddenInputFileName << std::endl;
+		return -1;
+	}
+
+	std::string	hiddenInputHeading;
+	hiddenFile >> hiddenInputHeading;
+	if (hiddenInputHeading != p3Heading && hiddenInputHeading != p2Heading) {
+		std::cout << "Heading: " << hiddenInputHeading << " not supported." << std::endl;
+		return -1;
+	}
+
+	int hiddenWidth, hiddenHeight, hiddenPixelCount;
+	hiddenFile >> hiddenWidth >> hiddenHeight >> maxVal;;
+	hiddenPixelCount = hiddenWidth * hiddenHeight;
+	std::cout << "Reading " + hiddenInputHeading + " file with width: " << hiddenWidth << " and hiddenHeight: " << hiddenHeight << std::endl;
+
+	maxVal = 255;
+
+	if (originalInputHeading == p3Heading) {
+		rgb* originalPixelArray = new rgb[originalPixelCount];
 
 		rgb pixel;
-		for (int i = 0; i < pixelCount; i++) {
-			inFile >> pixel.r >> pixel.g >> pixel.b;
-			pixelArray[i] = pixel;
+		for (int i = 0; i < originalPixelCount; i++) {
+			originalFile >> pixel.r >> pixel.g >> pixel.b;
+			originalPixelArray[i] = pixel;
 		}
 
-		inFile.close();
+		originalFile.close();
 
-		rgb* newPixelArray = new rgb[pixelCount];
+		rgb* hiddenPixelArray = new rgb[hiddenPixelCount];
+
+		for (int i = 0; i < hiddenPixelCount; i++) {
+			hiddenFile >> pixel.r >> pixel.g >> pixel.b;
+			hiddenPixelArray[i] = pixel;
+		}
+
+		hiddenFile.close();
+
+		int* hiddenBits = new int[hiddenPixelCount * 4 * 3];
+		rgb tempPixel;
+		int tempVal;
+		int hiddenBitsIndex = 0;
+		for (int i = 0; i < hiddenPixelCount; i++) {
+			tempPixel = hiddenPixelArray[i];
+
+			tempVal = tempPixel.r;
+			for (int j = 0; j < 4; j++) {
+				hiddenBits[hiddenBitsIndex++] = tempVal % 4;
+				tempVal /= 4;
+			}
+
+			tempVal = tempPixel.g;
+			for (int j = 0; j < 4; j++) {
+				hiddenBits[hiddenBitsIndex++] = tempVal % 4;
+				tempVal /= 4;
+			}
+
+			tempVal = tempPixel.b;
+			for (int j = 0; j < 4; j++) {
+				hiddenBits[hiddenBitsIndex++] = tempVal % 4;
+				tempVal /= 4;
+			}
+		}
+
+		rgb* newPixelArray = new rgb[originalPixelCount];
 		//modify pixelarray
-		for (int i = 0; i < pixelCount; i++) {
-			newPixelArray[i].r = lastNBits(pixelArray[i].r, n);
-			newPixelArray[i].g = lastNBits(pixelArray[i].g, n);
-			newPixelArray[i].b = lastNBits(pixelArray[i].b, n);
+		int halfNibbleIndex = 0;
+		for (int i = 0; i < originalPixelCount; i++) {
+			if (halfNibbleIndex <= hiddenBitsIndex) {
+				newPixelArray[i].r = originalPixelArray[i].r + hiddenBits[halfNibbleIndex++];
+				newPixelArray[i].g = originalPixelArray[i].g + hiddenBits[halfNibbleIndex++];
+				newPixelArray[i].b = originalPixelArray[i].b + hiddenBits[halfNibbleIndex++];
+			}
+			else {
+				newPixelArray[i] = originalPixelArray[i];
+			}
 		}
 		//output copy
-		std::string outputFileName = inputFileName.substr(0, inputFileName.length() - 4) + "Last" + std::to_string(n) + "Bits.ppm";
+		std::string outputFileName = originalInputFileName.substr(0, originalInputFileName.length() - 4) + "Plus" + hiddenInputFileName.substr(0, hiddenInputFileName.length() - 4) + ".ppm";
 		std::ofstream outFile;
 		outFile.open(outputFileName.c_str(), std::ios::out);
 
@@ -73,33 +137,35 @@ int main()
 
 		std::cout << "Writing...";
 		outFile << p3Heading << std::endl;
-		outFile << width << "	" << height << std::endl << maxVal << std::endl;
+		outFile << originalWidth << "	" << originalHeight << std::endl << maxVal << std::endl;
 
-		for (int i = 0; i < pixelCount; i++) {
+		for (int i = 0; i < originalPixelCount; i++) {
 			pixel = newPixelArray[i];
-			outFile << pixel.r << " " << pixel.g << " " << pixel.b << std::endl;
+			outFile << (int)pixel.r << " " << (int)pixel.g << " " << (int)pixel.b << std::endl;
 		}
 
 		outFile.close();
-		delete[] pixelArray;
+		delete[] newPixelArray;
 
-	} else if (inputHeading == p2Heading) {
-		int* pixelArray = new int[pixelCount];
+	}
+	/*
+	else if (originalInputHeading == p2Heading) {
+		int* pixelArray = new int[originalPixelCount];
 
-		for (int i = 0; i < pixelCount; i++) {
+		for (int i = 0; i < originalPixelCount; i++) {
 			inFile >> pixelArray[i];
 		}
 
 		inFile.close();
 
 		for (int n = 1; n < 8; n++) {
-			int* newPixelArray = new int[pixelCount];
+			int* newPixelArray = new int[originalPixelCount];
 			//modify pixelarray
-			for (int i = 0; i < pixelCount; i++) {
+			for (int i = 0; i < originalPixelCount; i++) {
 				newPixelArray[i] = lastNBits(pixelArray[i], n);
 			}
 			//output copy
-			std::string outputFileName = inputFileName.substr(0, inputFileName.length() - 4) + "Last" + std::to_string(n) + "Bits.pgm";
+			std::string outputFileName = originalInputFileName.substr(0, originalInputFileName.length() - 4) + "Last" + std::to_string(n) + "Bits.pgm";
 			std::ofstream outFile;
 			outFile.open(outputFileName.c_str(), std::ios::out);
 
@@ -110,16 +176,18 @@ int main()
 
 			std::cout << "Writing...";
 			outFile << p2Heading << std::endl;
-			outFile << width << "	" << height << std::endl << maxVal << std::endl;
+			outFile << width << "	" << originalHeight << std::endl << maxVal << std::endl;
 
-			for (int i = 0; i < pixelCount; i++) {
-				outFile << newPixelArray[i] << std::endl;
+			for (int i = 0; i < originalPixelCount; i++) {
+				outFile << (int)newPixelArray[i] << std::endl;
 			}
 
 			outFile.close();
+			delete[] newPixelArray;
 		}
 		delete[] pixelArray;
 	}
+	*/
 }
 
 
@@ -160,4 +228,8 @@ int lastNBits(int value, int nBitIndex) {
 		bin[i] = 0;
 	}
 	return binToDecimal(bin);
+}
+
+int removeNBits(int value, int numBits) {
+	return value - (value % powerOf(2, numBits));
 }
